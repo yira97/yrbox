615e38344714cbf07cf29984c5696c63ab37eda8
yrfg <percivalstr@163.com>
2020-03-15 02:01:38 +0800

    connect db

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ const prog = new program.Command();
 
 prog
   .version('0.0.1')
-  .command('media', '媒体处理', {executableFile: 'lib/media/entry.js'})
+  .command('media', '媒体处理', { executableFile: 'lib/media/entry.js' })
+  .command('routine', '日常工作', { executableFile: 'lib/routine/entry.js' })
   .parse(process.argv);
